8afcb96c3f8dc0a8f2e0d89bbc90c256cb771b94
adrbessy <adrienbessy@hotmail.com>
2021-08-04 20:01:44 +0200

    generate jacoco ut report: 82% of coverage

--- src/main/java/com/mediscreen/model/Note.java
+++ src/main/java/com/mediscreen/model/Note.java
@@ -5,12 +5,10 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
